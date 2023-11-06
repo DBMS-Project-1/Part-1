d280c1a9615ec108c32ecaaa63b732655f748893
Issa <98367269+IssaPortfolio@users.noreply.github.com>
2023-11-05 19:40:13 -0500

    Update sqlScript.sql

--- sqlScript.sql
+++ sqlScript.sql
@@ -1,19 +1,28 @@
 USE testdb;
-drop table if exists Role; 
 drop table if exists User; 
+drop table if exists Quote; 
+drop table if exists Role; 
 
 CREATE TABLE Role (
     role_id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(255) NOT NULL UNIQUE
 );
 
+CREATE TABLE Quote (
+	quote_id INT AUTO_INCREMENT PRIMARY KEY,
+    num_of_trees INT,
+    budget DECIMAL (10, 2)
+);
 CREATE TABLE User (
     unique_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role_id INT,
-    FOREIGN KEY (role_id) REFERENCES Role(role_id)
+    quote_id INT,
+    FOREIGN KEY (role_id) REFERENCES Role(role_id),
+	FOREIGN KEY (quote_id) REFERENCES Quote(quote_id)
 );
+
 
 INSERT INTO Role (role_name)
 VALUES 
@@ -36,5 +45,9 @@ VALUES
 
 SELECT * FROM Role;
 SELECT * FROM User;
+SELECT * FROM Quote;
 
-delete from User
+
+delete from User;
+delete from Role;
+delete from Quote;
